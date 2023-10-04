75cba8e4032784fa543f595fa3b5b713e3cc315e
ogechukwu1 <desunshine0@gmail.com>
2023-10-04 20:46:58 +0100

    upates

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+####################################################################################################################
+##### This automates the installation and configuring of apache webserver to listen on port 8000
+##### Usage: Call the script and pass in the Public_IP of your EC2 instance as the first argument as shown below:
+######## ./install_configure_apache.sh 127.0.0.1
+####################################################################################################################
+
+set -x # debug mode
+set -e # exit the script if there is an error
+set -o pipefail # exit the script when there is a pipe failure
+
+PUBLIC_IP=$1
+
+[ -z "${PUBLIC_IP}" ] && echo "Please pass the public IP of your EC2 instance as an argument to the script" && exit 1
+
+sudo apt update -y &&  sudo apt install apache2 -y
+
+sudo systemctl status apache2
+
+if [[ $? -eq 0 ]]; then
+    sudo chmod 777 /etc/apache2/ports.conf
+    echo "Listen 8000" >> /etc/apache2/ports.conf
+    sudo chmod 777 -R /etc/apache2/
+
+    sudo sed -i 's/<VirtualHost \*:80>/<VirtualHost *:8000>/' /etc/apache2/sites-available/000-default.conf
+
+fi
+sudo chmod 777 -R /var/www/
+echo "<!DOCTYPE html>
+        <html>
+        <head>
+            <title>My EC2 Instance</title>
+        </head>
+        <body>
+            <h1>Welcome to my EC2 instance</h1>
+            <p>Public IP: "${PUBLIC_IP}"</p>
+        </body>
+        </html>" > /var/www/html/index.html
+
+sudo systemctl restart apache2
+
